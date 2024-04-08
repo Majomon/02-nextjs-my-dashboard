62b9a10594837f2ee26b30d5958507582cc4b423
Mauri <mauri.monzon91@gmail.com>
2024-04-08 10:20:40 -0300

    priority

--- src/app/pokemons/components/PokemonCard.tsx
+++ src/app/pokemons/components/PokemonCard.tsx
@@ -15,18 +15,19 @@ export const PokemonCard = ({ pokemon }: Props) => {
       <div className="flex  flex-col bg-white rounded overflow-hidden shadow-lg">
         <div className="flex flex-col items-center justify-center text-center p-6 bg-gray-800 border-b">
           <Image
-            key={pokemon.id}
+            key={id}
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
             width={100}
             height={100}
-            alt={pokemon.name}
+            alt={name}
+            priority={false}
           />
           <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">
-            {pokemon.name}
+            {name}
           </p>
           <div className="mt-5">
             <Link
-              href={`dashboard/pokemon/${id}`}
+              href={`/pokemon/${id}`}
               className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
             >
               Más información
