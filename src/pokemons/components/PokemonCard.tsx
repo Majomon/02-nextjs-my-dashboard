9e8b40bc86673dc94283bbc372cf16ec7f7bf373
Mauri <mauri.monzon91@gmail.com>
2024-04-08 22:03:18 -0300

    favoritos

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { toggleFavorite } from "@/store/pokemons/pokemons";
 import Image from "next/image";
 import Link from "next/link";
-import { IoHeartOutline } from "react-icons/io5";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
 import { SimplePokemon } from "../interfaces/simple-pokemon";
 
 interface Props {
@@ -9,6 +12,14 @@ interface Props {
 
 export const PokemonCard = ({ pokemon }: Props) => {
   const { id, name } = pokemon;
+  // Doble negación para transformar a un valor booleano
+  const isFavorite = useAppSelector((state) => !!state.pokemons[id]);
+  /* const isFavorite = useAppSelector((state) => state.pokemons[id]!== undefined); */
+  const dispath = useAppDispatch();
+
+  const onToggle = () => {
+    dispath(toggleFavorite(pokemon));
+  };
 
   return (
     <div className="mx-auto right-0 mt-2 w-60 bg-red-900">
@@ -35,20 +46,20 @@ export const PokemonCard = ({ pokemon }: Props) => {
           </div>
         </div>
         <div className="border-b">
-          <Link
-            href="/dashboard/main"
-            className="px-4 py-2 hover:bg-gray-100 flex items-center"
+          <div
+            onClick={onToggle}
+            className="px-4 py-2 hover:bg-gray-100 flex items-center cursor-pointer"
           >
             <div className="text-red-600">
-              <IoHeartOutline />
+              {isFavorite ? <IoHeart /> : <IoHeartOutline />}
             </div>
             <div className="pl-3">
               <p className="text-sm font-medium text-gray-800 leading-none">
-                No es favorito
+                {isFavorite ? "Es favorito" : "No es favorito"}
               </p>
-              <p className="text-xs text-gray-500">View your campaigns</p>
+              <p className="text-xs text-gray-500">Click para cambiar</p>
             </div>
-          </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -1,19 +1,30 @@
 import { SimplePokemon } from "@/pokemons";
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface PokemonsState {
   [key: string]: SimplePokemon;
 }
 const initialState: PokemonsState = {
   "1": { id: "1", name: "bulbasaur" },
+  "3": { id: "3", name: "asdasd" },
 };
 
 const pokemonsSlice = createSlice({
   name: "pokemons",
   initialState,
-  reducers: {},
+  reducers: {
+    toggleFavorite(state, action: PayloadAction<SimplePokemon>) {
+      const pokemon = action.payload;
+      const { id } = pokemon;
+      if (!!state[id]) {
+        delete state[id];
+        return;
+      }
+      state[id] = pokemon;
+    },
+  },
 });
 
-export const {} = pokemonsSlice.actions;
+export const { toggleFavorite } = pokemonsSlice.actions;
 
 export default pokemonsSlice.reducer;
